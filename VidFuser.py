210ffeef1ee97c8628d47c6a7ae970c86af64d36
Joefer Traya <125757323+jNsatiable@users.noreply.github.com>
2023-03-17 01:15:16 +0800

    Added output name entry which is modifiable

--- VidFuser.py
+++ VidFuser.py
@@ -7,42 +7,36 @@ import os, sys, time
 clips = []
 
 def specify_src():
-    global src_folder
-    src_folder = filedialog.askdirectory()
+    #global src_folder_init
+    src_folder_init = filedialog.askdirectory()
     spec_src_entry.delete(0, END)
-    spec_src_entry.insert(END,src_folder)
-    return src_folder
+    spec_src_entry.insert(END,src_folder_init)
+    #return src_folder_init
 
 def specify_dest():
-    global dest_folder
-    dest_folder = filedialog.askdirectory()
-    return dest_folder
+    #global dest_folder_init
+    dest_folder_init = filedialog.askdirectory()
+    spec_dest_entry.delete(0, END)
+    spec_dest_entry.insert(END,dest_folder_init)    
+    #return dest_folder_init
 
 def fuse_vids():
-    spec_src_entry.get()
-    global src_folder, dest_folder
-
-    try:
-        src_folder = src_folder
-    except NameError:
-        src_folder = os.getcwd()
-
+    src_folder = spec_src_entry.get()
+    dest_folder = spec_dest_entry.get()
+    
+    #print(f'src_folder: {src_folder}\ndest_folder: {dest_folder}')
+    
     for filename in os.listdir(src_folder):
         try:
             vid_file = VideoFileClip(os.path.join(src_folder,filename))
             clips.append(vid_file)
         except Exception as e:
-            print(f'Skipping {filename} as it is not a valid file.')
-
-    try:
-        dest_folder = dest_folder
-    except NameError:
-        dest_folder = os.getcwd()
+            print(f'Skipping {filename} as it is not a valid video file.')
     
     if len(clips) > 1:            
         print(f'Stitching {len(clips)} videos...')
-        #stitched = concatenate_videoclips(clips)
-        #stitched.write_videofile(os.path.join(dest_folder,'stitched_vids.mp4'))
+        stitched = concatenate_videoclips(clips)
+        stitched.write_videofile(os.path.join(dest_folder, output_name_entry.get()))
         print(f'Done. You may now quit the app.')
         input()
     elif len(clips) == 1:
@@ -50,33 +44,28 @@ def fuse_vids():
         time.sleep(1)
         print(f'Exiting app...')
         time.sleep(1)
+        root.destroy()
+        sys.exit()
     else:
         print(f'No videos found.')
         time.sleep(1)
         print(f'Exiting app...')
         time.sleep(1)
-        raise Exception
+        root.destroy()
         sys.exit()
 
 root = Tk()
 root.title('VidFuser')
 root.iconbitmap('resources/VidFuser.ico')
-root.geometry('500x300')
-
-#lf = LabelFrame(root, text = 'VidFuser')
-#lf.pack(padx=20)
-#my_frame = Frame(root, bg="green")
-#my_frame.pack(pady=10)
-
+root.geometry('400x150')
 
 photo = Image.open('resources/folder.ico')
 photo = photo.resize((15,15), Image.ANTIALIAS)
-
 img = ImageTk.PhotoImage(photo)
 
 # -- Source Folder
-spec_src_lbl1 = Label(root, text="Source Folder:")
-spec_src_lbl1.grid(row=0, column=0)
+spec_src_lbl = Label(root, text="Source Folder:")
+spec_src_lbl.grid(row=0, column=0)
 
 spec_src_entry = Entry(root, width=40)
 spec_src_entry.grid(row=0, column=1)
@@ -86,21 +75,22 @@ spec_src_button = Button(root, text="Browse", image=img, command = specify_src)
 spec_src_button.grid(row=0, column=2, padx=5)
 
 # -- Destination Folder
-spec_dest_lbl1 = Label(root, text="Target Folder:")
-spec_dest_lbl1.grid(row=1, column=0)
-
+spec_dest_lbl = Label(root, text="Target Folder:")
+spec_dest_lbl.grid(row=1, column=0)
 spec_dest_entry = Entry(root, width=40)
 spec_dest_entry.grid(row=1, column=1)
 spec_dest_entry.insert(END, os.getcwd())
-
-spec_dest_button = Button(root, text="Browse", image=img, command = specify_src)
+spec_dest_button = Button(root, text="Browse", image=img, command = specify_dest)
 spec_dest_button.grid(row=1, column=2, padx=5)
 
+# -- Output Name
+output_name_lbl = Label(root, text="Output Name:")
+output_name_lbl.grid(row=2, column=0)
+output_name_entry = Entry(root, width=40)
+output_name_entry.grid(row=2, column=1)
+output_name_entry.insert(END, "fused_vids.mp4")
 
-#spec_loc_button = Button(my_frame, text="Choose Destination", anchor="w", command = specify_dest)
-#spec_loc_button.grid(row=1, column=0, padx=10)
-
-#fuse_button = Button(my_frame, text="Start Fusing", command = fuse_vids)
-#fuse_button.grid(row=2, column=0, padx=10)
+fuse_button = Button(root, text="Start Fusing", command = fuse_vids)
+fuse_button.grid(row=3, column=1, pady=30)
 
 root.mainloop()
